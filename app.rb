fd09b30b315a76a47cd46ac23bcac56084b5aacb
deniznida <denizunat@hotmail.com>
2015-02-26 16:42:09 -0500

    done

--- app.rb
+++ app.rb
@@ -3,5 +3,18 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
     # define your routes here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :form
+    end
+
+    post '/pirates' do
+      @pirate = params[:pirate]
+      @ships = params[:ship]
+      erb :display 
+    end
   end
 end
